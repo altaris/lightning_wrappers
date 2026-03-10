3c2db0e15c4228895a92f345aabbf8528d3d1854
Cédric HT <altaris@users.noreply.github.com>
2026-03-10 16:15:04 +0900

    Pin memory of train DL by default

--- lightning_wrappers/base/dataset.py
+++ lightning_wrappers/base/dataset.py
@@ -4,6 +4,7 @@ DEFAULT_TRAIN_DATALOADER_KWARGS = {
     "shuffle": True,
     "batch_size": 64,
     "num_workers": 2,
+    "pin_memory": True,
 }
 DEFAULT_VAL_DATALOADER_KWARGS = {
     "batch_size": 64,
